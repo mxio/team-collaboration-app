adec230cec0883bb0e557de33898a5a6ce638d88
mxio <mxio@users.noreply.github.com>
2016-10-13 13:09:02 -0600

    Added object to hold all messages

--- chat.js
+++ chat.js
@@ -4,6 +4,15 @@ var log = document.getElementById('chat-log');
 function submit (text) {
 
 	log.innerHTML +=  `<li class="chat-record chat-record-theme-1"><a href="#!" class="chat-record-avatar chat-avatar"><img src="http://placekitten.com/64/64" alt=""></a><div class="chat-record-bubble"><div class="chat-record-meta"><a class="chat-record-meta-link" href="#!">16 minutes ago</a></div><div class="chat-record-heading"><h2 class="chat-record-heading">Fluffy McFlufferson</h2></div><div class="chat-record-content">` + document.getElementById('message-input').value; + `</div></div></li>`
+
+	messages.personOne.push(document.getElementById('message-input').value);
 }
 
+//change every other message to have its different theme
+
 //object to store messages
+
+messages = {
+	personOne: [
+	]
+}
