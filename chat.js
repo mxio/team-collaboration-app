dae46888efe11a5c1c3f148815c0fa70e0abf674
mxio <mxio@users.noreply.github.com>
2016-10-13 13:04:26 -0600

    Fixed message log functionality

--- chat.js
+++ chat.js
@@ -1,24 +1,9 @@
-var chatInput = document.getElementById('input').value;
 var log = document.getElementById('chat-log');
 
 //show message on the chat log
-function submit (input) {
-	log.innerHTML +=  `<li class="chat-record chat-record-theme-1">
-                <a href="#!" class="chat-record-avatar chat-avatar">
-                  <img src="http://placekitten.com/64/64" alt="">
-                </a>
-                <div class="chat-record-bubble">
-                  <div class="chat-record-meta">
-                    <a class="chat-record-meta-link" href="#!">16 minutes ago</a>
-                  </div>
-                  <div class="chat-record-heading">
-                  <h2 class="chat-record-heading">Fluffy McFlufferson</h2>
-                  </div>
-                  <div class="chat-record-content">` +
-                    chatInput +
-                  `</div>
-                </div>
-              </li>`
+function submit (text) {
+
+	log.innerHTML +=  `<li class="chat-record chat-record-theme-1"><a href="#!" class="chat-record-avatar chat-avatar"><img src="http://placekitten.com/64/64" alt=""></a><div class="chat-record-bubble"><div class="chat-record-meta"><a class="chat-record-meta-link" href="#!">16 minutes ago</a></div><div class="chat-record-heading"><h2 class="chat-record-heading">Fluffy McFlufferson</h2></div><div class="chat-record-content">` + document.getElementById('message-input').value; + `</div></div></li>`
 }
 
 //object to store messages
